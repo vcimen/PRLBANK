d0b253d439dd01e31689472308b4a0e3155244f4
kemalgurler <kemgur52@gmail.com>
2021-04-04 19:28:15 -0400

    April04

---
+++ src/test/java/gmibank/pages/US_003_Page.java
@@ -0,0 +1,12 @@
+package gmibank.pages;
+
+import gmibank.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class US_003_Page {
+    public US_003_Page (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+}
